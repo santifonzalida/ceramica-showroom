970622a0261f12c944ba74fedab3ed80c07d97d3
santifonzalida <santiifonzalida@gmail.com>
2023-10-04 15:45:51 -0300

    new image section to upload

--- src/Components/Products/createProduct.jsx
+++ src/Components/Products/createProduct.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { PhotoIcon } from '@heroicons/react/24/solid'
 
 const CreateProduct = ({setMostrarCrear}) => {
 
@@ -25,15 +26,16 @@ const CreateProduct = ({setMostrarCrear}) => {
         console.log(producto);
     };
 
-    const [imagen, setImagen] = useState(null);
-    const [imagenBase64, setImagenBase64] = useState('');
+    const [imagenes, setImagenes] = useState(['', '', '']); // Inicialmente, 3 elementos vacíos
 
-    const handleImagenChange = (e) => {
+    const handleImagenChange = (e, index) => {
       const archivo = e.target.files[0];
       if(archivo) {
         const reader = new FileReader();
         reader.onload = (event) => {
-            setImagenBase64(event.target.result);
+            const nuevasImagenes = [...imagenes];
+            nuevasImagenes[index] = event.target.result;
+            setImagenes(nuevasImagenes);
         };
         reader.readAsDataURL(archivo);
         console.log(reader);
@@ -74,6 +76,40 @@ const CreateProduct = ({setMostrarCrear}) => {
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">
+                        Seleccionar imágenes
+                    </label>
+
+                    <div className="bg-gray-100 p-4">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                            {imagenes.map((imagen, index) => (
+                            <div key={index} className="bg-white p-4 rounded-lg border border-gray-300 flex flex-col items-center justify-center">
+                                {imagen ? (
+                                    <img src={imagen} alt={`Imagen ${index}`} className="max-h-36 max-w-full rounded-md" />
+                                ) : (
+                                <div className="text-4xl text-gray-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                                    </svg>
+                                </div>
+                                )}
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={(e) => handleImagenChange(e, index)}
+                                    className="hidden"
+                                    id={`imagenInput-${index}`}
+                                />
+                                <label htmlFor={`imagenInput-${index}`} className={`${imagen ? 'hidden' : ''} mt-2 cursor-pointer text-blue-500`}>
+                                    Cargar Imagen
+                                </label>
+                            </div>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">
                         Precio
                     </label>
                     <input
@@ -100,22 +136,7 @@ const CreateProduct = ({setMostrarCrear}) => {
                         required
                     />
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">
-                        Imagen URL
-                    </label>
-                    <input
-                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        id="imagen"
-                        name="imagen"
-                        value={producto.imagen}
-                        onChange={handleChange}
-                        required
-                    />
-                    
-                </div>
-                <div className="mb-6 text-center">
+                <div className="mb-6 text-right">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit"
@@ -127,8 +148,6 @@ const CreateProduct = ({setMostrarCrear}) => {
                         onClick={() => setMostrarCrear(false)}>Cancelar</button>
                 </div>
             </form>
-            <input type="file" accept="image/*" onChange={handleImagenChange} />
-            {imagenBase64 && <img src={imagenBase64} alt="Imagen seleccionada" />}
         </div>
     );
 }
