2a3b25f1145c23aad72abf1a45912bfa296594d2
santifonzalida <santiifonzalida@gmail.com>
2023-11-02 15:29:35 -0300

    page icon title

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -33,24 +33,22 @@ const ProductsCRUD = () => {
     },[]);
 
     return (
-        <div>
-            <div className="bg-gray-100 font-sans">
-                <div className="container mx-auto p-8">
-                    <div className="flex items-center mb-2" >
-                        <h1 className="text-3xl font-semibold">Lista de Productos</h1>
-                        <button className={`${mostrarCrear ? 'hidden' : ''} bg-blue-500 text-white px-2 py-1 ml-5 rounded`} onClick={() => setMostrarCrear(true)}>Nuevo</button>
-                    </div>
-                    { showSpinner ? <Spinner /> : 
-                        <div>
-                            <div className={`${mostrarCrear ? '' : 'hidden'} row`}>
-                                <CreateProduct setMostrarCrear={setMostrarCrear} products={productos} setProducts={setProductos}/>
-                            </div>
-                            <div className={`${mostrarCrear ? 'hidden' : 'row'}`}>
-                                <TableProducts products={productos} setProducts={setProductos} />
-                            </div>
-                        </div>
-                    }
+        <div className="bg-gray-100 font-sans">
+            <div className="container mx-auto p-8">
+                <div className="flex items-center mb-2" >
+                    <h1 className="text-3xl font-semibold">Lista de Productos</h1>
+                    <button className={`${mostrarCrear ? 'hidden' : ''} bg-blue-500 text-white px-2 py-1 ml-5 rounded`} onClick={() => setMostrarCrear(true)}>Nuevo</button>
                 </div>
+                { showSpinner ? <Spinner /> : 
+                    <div>
+                        <div className={`${mostrarCrear ? '' : 'hidden'} row`}>
+                            <CreateProduct setMostrarCrear={setMostrarCrear} products={productos} setProducts={setProductos}/>
+                        </div>
+                        <div className={`${mostrarCrear ? 'hidden' : 'row'}`}>
+                            <TableProducts products={productos} setProducts={setProductos} />
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     );
