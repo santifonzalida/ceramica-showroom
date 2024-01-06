0c48d155efc7f26101d96683f08d530b3b3e00ab
santifonzalida <santiifonzalida@gmail.com>
2024-01-06 14:16:23 -0300

    dropdown button for config navigation

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -52,15 +52,56 @@ const Navbar = () => {
                         className={({isActive}) => isActive ? activeStyle : undefined}
                     > Todo </NavLink>
                 </li>
-                <li className={`${location.pathname === '/dashboard' ? '' : 'hidden'} font-semibold`}>
-                    <a>Configuración &gt; </a>
-                </li>
-                <li className={`${location.pathname === '/dashboard' ? '' : 'hidden'}`}>
-                    <a className="cursor-pointer" onClick={onProductoClick}>Productos</a>
-                </li>
-                <li className={`${location.pathname === '/dashboard' ? '' : 'hidden'}`}>
-                    <a className="cursor-pointer" onClick={onCateogiriaClick}>Categorias</a>
-                </li>
+                {
+                    location.pathname === '/dashboard' ?
+                    <div
+                        className="relative font-semibold"
+                        data-te-dropdown-ref
+                        data-te-dropdown-alignment="end">
+                        <a
+                            className="flex items-center whitespace-nowrap cursor-pointer px-6 pb-2 pt-2.5 font-bold text-black  motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                            id="dropdownMenuConfig"
+                            data-te-dropdown-toggle-ref
+                            aria-expanded="false"
+                            data-te-ripple-init
+                            data-te-ripple-color="light"> 
+                            Configuración
+                            <span className="ml-2 w-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5">
+                                    <path
+                                    fillRule="evenodd"
+                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                    clipRule="evenodd" />
+                                </svg>
+                            </span>
+                        </a>
+                        <ul
+                            className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+                            aria-labelledby="dropdownMenuConfig"
+                            data-te-dropdown-menu-ref>
+                            <li>
+                                <a
+                                    className="block w-full whitespace-nowrap bg-transparent cursor-pointer px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    data-te-dropdown-item-ref
+                                    onClick={onProductoClick}
+                                >Productos</a>
+                            </li>
+                            <li>
+                                <a
+                                    className="block w-full whitespace-nowrap bg-transparent cursor-pointer px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    data-te-dropdown-item-ref
+                                    onClick={onCateogiriaClick}
+                                >Categoria</a>
+                            </li>
+                        </ul>
+                    </div>
+                    : ''
+                }
+                
                 {
                     location.pathname != '/dashboard' ?
                     categories?.map((cat) => (
